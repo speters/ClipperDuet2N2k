417a9d8a8ae180681dc992db154cc7301000a83d
Soenke J. Peters <peters__git@opcenter.de>
2024-02-19 11:16:42 +0100

    OS-independent git describe via dulwich Python module

--- version.py
+++ version.py
@@ -1,19 +1,48 @@
 # SPDX-License-Identifier: MIT
+
+# see https://community.platformio.org/t/how-to-build-got-revision-into-binary-for-version-output/15380/6
+
+import pkg_resources
+
+Import("env")
+
+required_pkgs = {'dulwich'}
+installed_pkgs = {pkg.key for pkg in pkg_resources.working_set}
+missing_pkgs = required_pkgs - installed_pkgs
+
+if missing_pkgs:
+    env.Execute('"$PYTHONEXE" -m pip install dulwich')
+
 import subprocess
 import datetime
 import os
 
-Import("env")
+from contextlib import suppress
+from dulwich import porcelain
+from dulwich.errors import NotGitRepository
 
-def get_firmware_specifier_build_flag():
-    try:
-        ret = subprocess.run(["git", "describe", "--always", "--dirty"], stdout=subprocess.PIPE, text=True)
-    except FileNotFoundError:
-        # git not found
-        return ("")
-    g =  ret.stdout.strip()
+# returns a version description like "git describe --always --dirty" would
+def get_git_describe_always_dirty(repo=None):
+    repo = repo or os.getcwd()
+    with suppress(NotGitRepository):
+        revid = None
+        with porcelain.open_repo_closing(repo) as r:
+            revid = r.head().decode("ascii")
+        version = porcelain.describe(repo, abbrev=9)
+        # version = revid[0:9] if version[0] == 'g' else version
+        status = porcelain.status(repo=repo, untracked_files="no")
+        dirtymark = "-dirty" if status.staged["add"] or status.staged["delete"] or status.staged["modify"] or status.unstaged else ""
+        # return f"{version}~git:{revid}{dirtymark}"
+        return f"{version}{dirtymark}"
+    return "unknown"
+
+# construct a pseudo-version string from git describe, which follows format requirements of NMEA2000
+def make_n2k_version(git_dirty_describe):
+    g = git_dirty_describe
     if g[0] == 'v':
         s = g[1:]
+    elif g == "unknown":
+        s = "0.0.0"
     else:
         s = g
 
@@ -33,18 +62,25 @@ def get_firmware_specifier_build_flag():
             t += "." + p[1]
         else:
             t += "." + "0"
+    return t
+
+def get_firmware_specifier_build_flag():
+    g =  get_git_describe_always_dirty('.')
 
     x = datetime.datetime.now()
     d = x.strftime("%Y-%m-%d")
+
+    t = make_n2k_version(g)
 
     n2kv = t + "\ \(" + d + "\)"
 
     # TODO:  env.StringifyMacro()
     build_flag = "-D N2K_SOFTWARE_VERSION=\\\"" + n2kv + "\\\" -D GIT_DESCRIBE=\\\"" + g + "\\\""
-    print ("Firmware Revision: " + t)
+    print ("Firmware Revision for N2k: " + t)
     print ("git Describe: " + g)
     return (build_flag)
 
+# This is a convenience-function for _me_
 def before_upload(source, target, env):
     # killall minicom
     if (os.environ.get("USER")=="soenke"):
